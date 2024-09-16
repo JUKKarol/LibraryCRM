410077995a58295043398537c8a213620c7174e9
JUKKarol <karolprzybylak2@gmail.com>
2024-09-16 21:40:03 +0200

    Init library controller

---
+++ api/LibraryCRM.API/Controllers/LibraryController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryCRM.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LibraryController(IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<LibraryDTO>>> GetLibraries()
+    {
+        var libraries = await mediator.Send(new GetAllLibrariesQuery());
+        return Ok(libraries);
+    }
+
+    [HttpGet("{libraryId}")]
+    public async Task<ActionResult<LibraryDTO>> GetLibrary([FromRoute] Guid libraryId)
+    {
+        var library = await mediator.Send(new GetLibraryByIdQuery(libraryId));
+    }
+}
