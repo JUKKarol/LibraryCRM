70325803c42d2848620a10ab5ce8c7261a096bab
JUKKarol <karolprzybylak2@gmail.com>
2024-09-19 22:25:05 +0200

    Add create book CQRS

---
+++ api/LibraryCRM.Application/Libraries/Commands/CreateLibraryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace LibraryCRM.Application.Libraries.Commands;
+
+public class CreateLibraryCommand : IRequest<Guid>
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+}
---
+++ api/LibraryCRM.Application/Libraries/Commands/CreateLibraryCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCRM.Application.Libraries.Commands;
+
+public class CreateLibraryCommandValidator : AbstractValidator<CreateLibraryCommand>
+{
+    public CreateLibraryCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Address)
+            .NotEmpty()
+            .MaximumLength(200);
+    }
+}
