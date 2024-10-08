93c256e6248ec806c09ffa5cba1009ee503cc771
JUKKarol <karolprzybylak2@gmail.com>
2024-10-08 15:01:38 +0200

    Move builder configuration to extension

--- api/LibraryCRM.API/Program.cs
+++ api/LibraryCRM.API/Program.cs
@@ -1,49 +1,18 @@
+using LibraryCRM.API.Extensions;
 using LibraryCRM.API.Middlewares;
 using LibraryCRM.Application.Extensions;
 using LibraryCRM.Domain.Entities;
 using LibraryCRM.Infrastructure.Extensions;
 using LibraryCRM.Infrastructure.Seeders;
-using Microsoft.OpenApi.Models;
-using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddPresentation();
+
 builder.Configuration.GetConnectionString("LibraryCRMDb");
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "bearerAuth"
-                }
-            },
-            []
-        }
-    });
-});
-
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration)
-);
 
 var app = builder.Build();
 
