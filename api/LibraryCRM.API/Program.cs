2e8573d081ef7c917e677ce7b1a0c9d47e6cc8fb
JUKKarol <karolprzybylak2@gmail.com>
2024-10-22 21:40:49 +0200

    Add EmailSender to DI

--- api/LibraryCRM.API/Program.cs
+++ api/LibraryCRM.API/Program.cs
@@ -1,9 +1,11 @@
 using LibraryCRM.API.Extensions;
 using LibraryCRM.API.Middlewares;
 using LibraryCRM.Application.Extensions;
+using LibraryCRM.Application.Services;
 using LibraryCRM.Domain.Entities;
 using LibraryCRM.Infrastructure.Extensions;
 using LibraryCRM.Infrastructure.Seeders;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,9 @@ builder.Configuration.GetConnectionString("LibraryCRMDb");
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
+
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration);
 
 var app = builder.Build();
 
