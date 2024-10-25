976105ac6d32b8ea6d71ea48b6955d20869516cf
JUKKarol <karolprzybylak2@gmail.com>
2024-10-25 23:14:16 +0200

    Add roles to seeder

--- api/LibraryCRM.Infrastructure/Seeders/LibrarySeeder.cs
+++ api/LibraryCRM.Infrastructure/Seeders/LibrarySeeder.cs
@@ -1,6 +1,8 @@
 ﻿using Bogus;
+using LibraryCRM.Domain.Constants;
 using LibraryCRM.Domain.Entities;
 using LibraryCRM.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
 
 namespace LibraryCRM.Infrastructure.Seeders;
 
@@ -16,7 +18,26 @@ internal class LibrarySeeder(LibraryDbContext db) : ILibrarySeeder
                 await db.Libraries.AddRangeAsync(libraries);
                 await db.SaveChangesAsync();
             }
+
+            if (!db.Roles.Any())
+            {
+                var roles = GetRoles();
+                await db.Roles.AddRangeAsync(roles);
+                await db.SaveChangesAsync();
+            }
         }
+    }
+
+    private IEnumerable<IdentityRole> GetRoles()
+    {
+        List<IdentityRole> roles =
+        [
+            new (UserRoles.User),
+            new (UserRoles.Owner),
+            new (UserRoles.Admin),
+        ];
+
+        return roles;
     }
 
     private List<Library> GetLibraries(int objectsToGenerate)
